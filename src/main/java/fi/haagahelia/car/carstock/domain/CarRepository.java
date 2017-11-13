e6b64df70123856955c659b03e81d5c90b967eb4
juhahinkula <hinkula@gmail.com>
2017-11-13 17:48:37 +0200

    Queries added

--- src/main/java/fi/haagahelia/car/carstock/domain/CarRepository.java
+++ src/main/java/fi/haagahelia/car/carstock/domain/CarRepository.java
@@ -1,6 +1,16 @@
 package fi.haagahelia.car.carstock.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
+	List<Car> findByBrand(@Param("brand") String brand);
+	List<Car> findByColor(@Param("color") String color);
+	List<Car> findByYear(@Param("year") int year);
+
 }
+
