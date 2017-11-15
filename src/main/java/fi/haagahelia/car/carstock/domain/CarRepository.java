4c8d8767888eba4a8088f340720f6d6623875b8d
juhahinkula <hinkula@gmail.com>
2017-11-15 12:37:02 +0200

    new queries

--- src/main/java/fi/haagahelia/car/carstock/domain/CarRepository.java
+++ src/main/java/fi/haagahelia/car/carstock/domain/CarRepository.java
@@ -9,8 +9,9 @@ import org.springframework.data.rest.core.annotation.RestResource;
 @RestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 	List<Car> findByBrand(@Param("brand") String brand);
+	List<Car> findByModel(@Param("model") String model);
+	List<Car> findByFuel(@Param("fuel") String fuel);
 	List<Car> findByColor(@Param("color") String color);
 	List<Car> findByYear(@Param("year") int year);
-
 }
 
